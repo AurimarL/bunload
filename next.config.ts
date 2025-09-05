56374a569f8f67100a10e0bfef0a3e787ca5836b
Aurimar-Lopes <aurimar.lopes@statementmc.com>
2025-09-05 12:13:25 +0100

    impro

--- next.config.ts
+++ next.config.ts
@@ -1,13 +1,15 @@
 import path from "node:path";
 import { withPayload } from "@payloadcms/next/withPayload";
+import type { Configuration } from "webpack";
 
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   // Your Next.js config here
   experimental: {
-    reactCompiler: false,
+    reactCompiler: true,
   },
-  webpack: (webpackConfig: any) => {
+  webpack: (webpackConfig: Configuration) => {
+    webpackConfig.resolve ??= {}; // garante que resolve existe
     webpackConfig.resolve.extensionAlias = {
       ".cjs": [".cts", ".cjs"],
       ".js": [".ts", ".tsx", ".js", ".jsx"],
