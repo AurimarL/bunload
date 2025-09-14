7ad79b24769bdb7acffcd327cf3e809744e1cf0f
Aurimar-Lopes <aurimar.lopes@statementmc.com>
2025-09-14 23:49:32 +0100

    add vercel upload storage

--- src/payload.config.ts
+++ src/payload.config.ts
@@ -1,5 +1,6 @@
 import { postgresAdapter } from "@payloadcms/db-postgres";
 import { resendAdapter } from "@payloadcms/email-resend";
+import { vercelBlobStorage } from "@payloadcms/storage-vercel-blob";
 import { buildConfig } from "payload";
 import sharp from "sharp";
 import { Media } from "./payload/collections/Media";
@@ -24,7 +25,17 @@ export default buildConfig({
     push: false, // DO NOT USE ON PRODUCTION
   }),
   sharp,
-  plugins: [],
+  plugins: [
+    vercelBlobStorage({
+      enabled: true, // Optional, defaults to true
+      // Specify which collections should use Vercel Blob
+      collections: {
+        media: true,
+      },
+      // Token provided by Vercel once Blob storage is added to your Vercel project
+      token: process.env.BLOB_READ_WRITE_TOKEN,
+    }),
+  ],
   email: resendAdapter({
     defaultFromAddress: "send@aurimarlopes.com",
     defaultFromName: "Aurimar",
